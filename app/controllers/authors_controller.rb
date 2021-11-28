6287aa2796cad5c79cd161d511c6628395f4bd00
Aruna <aruna.evan15@gmail.com>
2021-11-27 16:40:11 -0800

    commit tree 8677e4f41ff5fdb5c4d736e4ecdee77c90769fe8

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,15 @@
 class AuthorsController < ApplicationController
-  
-  def show
-    author = Author.find(params[:id])
 
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid_response
+
+  def show
+    author = find_author
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
@@ -17,5 +18,16 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+
+  def find_author
+    Author.find(params[:id])
+  end
   
+  # def render_not_found_response
+  #   errors.add()
+  # end
+
+  def render_record_invalid_response(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,15 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid_response
+
   def show
-    post = Post.find(params[:id])
-    
+    post = find_post
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = find_post
+    post.update!(post_params)
     render json: post
   end
 
@@ -20,4 +19,11 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  def find_post
+    Post.find(params[:id])
+  end
+
+  def render_record_invalid_response(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
 end
